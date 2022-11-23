e5a5d859746bc8bba18a6e2b658e5442a8dae7dd
Muhammad Husnain Khurshid <87422670+husnain9@users.noreply.github.com>
2022-11-23 13:15:11 +0500

    Update settings.py

--- bookstore_api/settings.py
+++ bookstore_api/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bookstore_api.urls'
-# CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ALLOWED_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
@@ -135,7 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-CORS_ALLOWED_ORIGINS = ["*"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
